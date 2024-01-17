17fd573888b7b9c443b01ab1865a83bcaffb4f48
dk-victory-resultscx <donn.victory@reultscx.com>
2024-01-17 20:23:16 +0800

    Converted scripts to CTAS

--- sql/dashboards/acad_search_usage.sql
+++ sql/dashboards/acad_search_usage.sql
@@ -1,25 +1,27 @@
-SELECT
-    customer
-    ,timestamp_date AS event_date
-    ,DATE(timestamp_date) AS daily
-    ,DATE_SUB(DATE(timestamp_date), INTERVAL EXTRACT(DAYOFWEEK FROM DATE(timestamp_date))-2 DAY) AS weekly
-    ,DATE(EXTRACT(YEAR FROM DATE(timestamp_date)), EXTRACT(MONTH FROM DATE(timestamp_date)), 1) AS monthly
-    ,DATE(EXTRACT(YEAR FROM DATE(timestamp_date)), 
-                    CASE EXTRACT(Quarter FROM DATE(timestamp_date))
-                            WHEN 1 THEN 1
-                            WHEN 2 THEN 4
-                            WHEN 3 THEN 7
-                            ELSE 9
-                    END, 1) AS quarterly
-    ,DATE(EXTRACT(YEAR FROM DATE(timestamp_date)), 1, 1) AS yearly
-    ,action
-    ,search_term
-    ,count
-    ,COUNT(action) AS action_count
-FROM analytics_gds.acad_search
-GROUP BY
-    customer
-    ,event_date
-    ,action
-    ,search_term
-    ,count
+CREATE TABLE analytics_rpt.acad_search_usage AS (
+    SELECT
+        customer
+        ,timestamp_date AS event_date
+        ,DATE(timestamp_date) AS daily
+        ,DATE_SUB(DATE(timestamp_date), INTERVAL EXTRACT(DAYOFWEEK FROM DATE(timestamp_date))-2 DAY) AS weekly
+        ,DATE(EXTRACT(YEAR FROM DATE(timestamp_date)), EXTRACT(MONTH FROM DATE(timestamp_date)), 1) AS monthly
+        ,DATE(EXTRACT(YEAR FROM DATE(timestamp_date)), 
+                        CASE EXTRACT(Quarter FROM DATE(timestamp_date))
+                                WHEN 1 THEN 1
+                                WHEN 2 THEN 4
+                                WHEN 3 THEN 7
+                                ELSE 9
+                        END, 1) AS quarterly
+        ,DATE(EXTRACT(YEAR FROM DATE(timestamp_date)), 1, 1) AS yearly
+        ,action
+        ,search_term
+        ,count
+        ,COUNT(action) AS action_count
+    FROM analytics_gds.acad_search
+    GROUP BY
+        customer
+        ,event_date
+        ,action
+        ,search_term
+        ,count
+)
